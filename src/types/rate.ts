18fff450d6250d47a92aad9501dfa9dadb08537d
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-02-02 00:32:03 +0000

    created initial rates API data

---
+++ src/types/rate.ts
@@ -0,0 +1,12 @@
+export type Rate = {
+  // _id: string;
+  name: string;
+  alias: string;
+  slug: string;
+  image: string;
+  currency: string;
+  rate: number;
+  amount: number;
+  note: string;
+  userRating: number;
+};
