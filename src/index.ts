8cf5944a3b8ea1a83dbd77abd2736488856c2391
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-08-02 18:35:16 +0100

    resolved cors error

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
 import { PrismaClient } from "@prisma/client";
 import { errorMiddleware } from "./middlewares/errors";
 
 const app: Express = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
